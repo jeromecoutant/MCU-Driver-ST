2ed34fe3588bc1056a09f9b58a1f8d2de2ea020c
Filip Jagodzinski <filip.jagodzinski@arm.com>
2021-06-23 22:30:15 +0200

    Test: Add a build_all, clean_all & run_all scripts

---
+++ sdfx_st/tests/build_all.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Copyright (c) 2021 Arm Limited
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TESTS_BUILT=()
+
+for TEST_PATH in $(find ./ -type f -name CMakeLists.txt); do
+    TEST_DIR_PATH=$(dirname "$TEST_PATH")
+    printf 'Building %s\n' "$TEST_DIR_PATH"
+    pushd "$TEST_DIR_PATH" || exit
+    cmake \
+        -S ./ \
+        -B ./cmake_build \
+        -G Ninja \
+        -DGREENTEA_CLIENT_STDIO=OFF \
+        -DMBED_TOOLCHAIN=GCC_ARM \
+        -DCMAKE_BUILD_TYPE=develop \
+        --log-level=ERROR \
+        || exit
+    cmake --build ./cmake_build || exit
+    popd || exit
+    TESTS_BUILT+=("$TEST_DIR_PATH")
+done
+
+printf '\nBUILD SUMMARY:\n'
+printf '%s\n' "${TESTS_BUILT[@]}"
---
+++ sdfx_st/tests/run_all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright (c) 2021 Arm Limited
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TESTS_RUN=()
+
+for TEST_BIN_PATH in $(find ./ -type f -name "sdfx-st-test-*.bin"); do
+    printf 'Running %s\n' "$TEST_BIN_PATH"
+    mbedhtrun \
+        --image-path="$TEST_BIN_PATH" \
+        --enum-host-tests="../../MCU-Driver-HAL/tests/host_tests/" \
+        --disk="/media/$USER/DIS_L4IOT/" \
+        --port="/dev/ttyACM0:115200" \
+        || exit
+    TESTS_RUN+=("$TEST_BIN_PATH")
+done
+
+printf '\nRUN SUMMARY:\n'
+printf '%s\n' "${TESTS_RUN[@]}"
